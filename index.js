b4a781040adc4ca021698cae9978723bf96dd71d
Severi Kujala <severi.kujala@hotmail.com>
2021-06-22 22:40:38 +0300

    complete ex3.3

--- index.js
+++ index.js
@@ -3,26 +3,26 @@ const app = express()
 
 
 let persons = [
-    {
-      "name": "Arto Hellas",
-      "number": "040-123456",
-      "id": 1
-    },
-    {
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "id": 2
-    },
-    {
-      "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": 3
-    },
-    {
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "id": 4
-    }
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  }
 ]
 
 
@@ -34,11 +34,27 @@ app.get("/info", (req, res) =>  {
 
 
 app.get("/api/persons", (req, res) => {
-    res.json(persons)
+  res.json(persons)
 })
+
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id
+  const person = persons.find(p => p.id == id)
+
+  if (person) {
+    res.json(person)
+
+  } else {
+    res.status(404).end()
+  }
+
+})
+
 
 const PORT = 3001
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
+
